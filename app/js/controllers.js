3fcd8927783d1a0d7c552623e79e8b2ab12897e0
Ana Dumitrache <>
2019-01-10 14:57:23 +0100

    added search result and erase address button

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,22 +5,68 @@ angular
 
 function MainController(dataService, geolocationService, $scope, $window) {
   $scope.filters = {};
-  $scope.filters.radius = "250";
+  $scope.filters.radius = 250;
+  $scope.filters.section = "";
+  $scope.coordinates = {};
+  $scope.recommendedPlaces = [];
+
+  $scope.venues = [];
+
+  $scope.radiusOptions = [250, 500, 1000, 10000];
 
   getUserLocation();
 
+  $scope.eraseLocation = function() {
+    $scope.coordinates.longitude = null;
+    $scope.coordinates.latitude = null;
+    $scope.filters.address = "";
+  };
+
+  $scope.getVenues = function() {
+    var currentPlace = $scope.autocomplete.getPlace();
+
+    if (currentPlace && currentPlace.geometry) {
+      $scope.coordinates.latitude = currentPlace.geometry.location.lat();
+      $scope.coordinates.longitude = currentPlace.geometry.location.lng();
+    }
+
+    if (!$scope.coordinates.longitude || !$scope.coordinates.latitude) {
+      $scope.recommendedPlaces.length = 0;
+    } else {
+      dataService
+        .getVenues({
+          longitude: $scope.coordinates.longitude,
+          latitude: $scope.coordinates.latitude,
+          radius: $scope.filters.radius,
+          section: $scope.filters.section
+        })
+        .then(function(httpResponse) {
+          $scope.recommendedPlaces = httpResponse.data.response.groups[0].items;
+        });
+    }
+  };
+
   function getUserLocation() {
     geolocationService.getCurrentPosition().then(function(response) {
+      $scope.coordinates = {
+        latitude: response.coords.latitude,
+        longitude: response.coords.longitude
+      };
       geolocationService
         .getAddressName(response.coords.latitude, response.coords.longitude)
         .then(function(result) {
           $scope.filters.address = result;
+          $scope.getVenues();
         });
     });
   }
 
+  $scope.updateRadius = function(radius) {
+    $scope.filters.radius = radius;
+  };
+
   $scope.autocompleteAddress = function() {
     var input = $window.document.getElementById("address");
-    new $window.google.maps.places.Autocomplete(input);
+    $scope.autocomplete = new $window.google.maps.places.Autocomplete(input);
   };
 }
--- app/js/services.js
+++ app/js/services.js
@@ -9,13 +9,14 @@ function dataService($http) {
   };
 
   function _getVenues(params) {
-    return $http.get({
+    return $http({
       url: "https://api.foursquare.com/v2/venues/explore",
       method: "GET",
       params: {
+        v: "20190110",
         client_id: "MUFW4S3QKMQBBEOW0M1I10HOL3XARPMM1D4MQYQ5OLJDFHBL",
         client_secret: "PRZND1SEVMDUZI0LKNQJON1H3DFLD1GPAIYBRVH4NFNZVSVP",
-        ll: params.longitude + "," + params.latitude,
+        ll: params.latitude + "," + params.longitude,
         radius: params.radius || 500,
         section: params.section || ""
       }
