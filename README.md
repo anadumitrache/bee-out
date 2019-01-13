1dd21b2663a276ff6dc6a186aa6ee2067f418e8d
Ana Dumitrache <>
2019-01-13 20:32:29 +0100

    added custom markers and centered map and readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# bee-out
-A simple web application that uses the Foursquare API
+# Bee Out
+
+A simple web application that uses the Foursquare API and provides some filters for searching points of interest.
+The use can open the application [here](https://anadumitrache.github.io/bee-out/app/)
+
+### Key features
+* When the user enters the application a pop-up will ask for sharing location aproval.
+* If the user accepts the Adress filter will pe filled with the user's current location, if not the address will be empty.
+* The user can type into the address input and a dropdown with more place will be available ( using Google Maps -> Place Autocomplete)
+* The user can delete the address by pressing the X button near the address field.
+* The user can choose a radius from the ones displayed.
+* The user can choose a category from the section dropdown.
+* When the user clicks search, all the filters are applied and a list with recommended places will be available in the following forms:
+* * By default, the List View is selected, displaying the data using a table. Each row will display the name of the place, the categories and the distance to it. If the user selects a row, he will be redirected to Google Maps.
+* * The user can click the Map View, and he map will be diplayed. The center of the map will be the adresses the  user entered and the recommended places will have a marker. If the user clicks the place marker, he will be prompted some information about the place (info from Foursquare) and also a link to be redirected to the google maps page.
+* If the API will not found any recommended place, the user will be displayed a message "No results."
+* While retrieving the data from the Foursquare servers the user will be prompted a spinner.
+* The user can use the application on Desktop or Mobile.
+
+
+![Desktop](README_Desktop_1.png) | ![Mobile](README_Mobile_1.png)
+------------ | -------------
+![Desktop](README_Desktop_2.png) | ![Mobile](README_Mobile_2.png)
+------------ | -------------
+![Desktop](README_Desktop_3.png) | ![Mobile](README_Mobile_3.png)
+
+### Project structure
+ 
+
+### Technical references
+Framework used:
+* Angular JS: https://code.angularjs.org/1.6.2/docs/guide
+
+Foursquare API:
+* Venues endpoint: https://developer.foursquare.com/docs/api/venues/explore
+* Authentication: https://developer.foursquare.com/docs/api/configuration/authentication
+
+Google Maps API:ç
+* Map : https://developers.google.com/maps/documentation/javascript/tutorial
+* Geolocation : https://developers.google.com/maps/documentation/javascript/geocoding
+* Autocomplete : https://developers.google.com/maps/documentation/javascript/examples/places-autocomplete
+* Markers: https://developers.google.com/maps/documentation/javascript/markers
+
+Design:
+* Bootstrap (4) CSS : https://getbootstrap.com
+* Material Design Icons: https://material.io/tools/icons
+* SVG created with: http://vectorpaint.yaks.co.nz/
+* Bee Icon: https://pixabay.com/en/bee-black-buzz-cute-gray-grey-1295432/
+* Hive Icon : https://www.flaticon.com/search?word=beehive 
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,122 +1,195 @@
 angular
-  .module("bee-out.controllers", [])
-  /* Main controller */
-  .controller("MainController", MainController);
+    .module("bee-out.controllers", [])
+    .controller("MainController", MainController);
 
 function MainController(dataService, geolocationService, $scope, $window) {
-  $scope.filters = {};
-  $scope.filters.radius = 250;
-  $scope.filters.section = "";
-  $scope.coordinates = {};
-  $scope.recommendedPlaces = [];
-  $scope.listView = true;
+    // store filters
+    $scope.filters = {
+        address: "",
+        radius: 250,
+        section: ""
+    };
 
-  var markers = [];
+    $scope.coordinates = {};
+    $scope.recommendedPlaces = [];
 
-  $scope.venues = [];
+    // flags to hide/sow the spinner,no-results tab and list-view
+    $scope.listView = true;
+    $scope.showSpinner = true;
+    $scope.noResults = false;
 
-  $scope.radiusOptions = [250, 500, 1000, 10000];
+    // array of map markers
+    $scope.markers = [];
+    // array of found venues
+    $scope.venues = [];
+    $scope.radiusOptions = [250, 500, 1000, 10000];
 
-  getUserLocation();
+    getUserLocation();
 
-  $scope.showList = function(value) {
-    $scope.listView = value;
-  };
+    // List view is selected by default
+    $scope.showList = function(value) {
+        $scope.listView = value;
+    };
 
-  function initMap() {
-    $scope.map = new $window.google.maps.Map(
-      $window.document.getElementById("map"),
-      {
-        zoom: 12,
-        center: {
-          lat: $scope.coordinates.latitude,
-          lng: $scope.coordinates.longitude
+    $scope.autocompleteAddress = function() {
+        var input = $window.document.getElementById("address");
+        $scope.autocomplete = new $window.google.maps.places.Autocomplete(input);
+    };
+
+    $scope.updateRadius = function(radius) {
+        $scope.filters.radius = radius;
+    };
+
+    $scope.eraseLocation = function() {
+        setUserCoordinates(null, null);
+        removeMarkers();
+        $scope.filters.address = "";
+        $scope.recommendedPlaces.length = 0;
+        $scope.noResults = true;
+    };
+
+    $scope.goToGoogleMaps = function(location) {
+        $window.open(
+            "https://www.google.com/maps?q=" + location.lat + "," + location.lng
+        );
+    };
+
+    // get the venues according to selected filters
+    $scope.getVenues = function() {
+        $scope.noResults = false;
+        var currentPlace = $scope.autocomplete.getPlace();
+
+        if (currentPlace && currentPlace.geometry) {
+            setUserCoordinates(
+                currentPlace.geometry.location.lat(),
+                currentPlace.geometry.location.lng()
+            );
         }
-      }
-    );
-  }
 
-  $scope.goToGoogleMaps = function(location) {
-    $window.open(
-      "https://www.google.com/maps?q=" + location.lat + "," + location.lng
-    );
-  };
+        if (
+            !$scope.autocomplete.getPlace ||
+            !$scope.coordinates.longitude ||
+            !$scope.coordinates.latitude ||
+            !$scope.filters.address
+        ) {
+            $scope.recommendedPlaces.length = 0;
+            $scope.noResults = true;
+        } else {
+            $scope.showSpinner = true;
+            dataService
+                .getVenues({
+                    longitude: $scope.coordinates.longitude,
+                    latitude: $scope.coordinates.latitude,
+                    radius: $scope.filters.radius,
+                    section: $scope.filters.section
+                })
+                .then(function(httpResponse) {
+                    $scope.showSpinner = false;
+                    $scope.recommendedPlaces = httpResponse.data.response.groups[0].items;
+                    addMarkersToMap();
+                    centerMap($scope.coordinates.latitude, $scope.coordinates.longitude);
+                    if ($scope.recommendedPlaces.length === 0) $scope.noResults = true;
+                });
+        };
 
-  function addMarkersToMap() {
-    removeMarkers();
-    $scope.recommendedPlaces.forEach(function(element) {
-      var venueLocation = element.venue.location;
-      var marker = new $window.google.maps.Marker({
-        position: { lat: venueLocation.lat, lng: venueLocation.lng },
-        map: $scope.map
-      });
-      $window.google.maps.event.addListener(marker, "click", function() {
-        $scope.goToGoogleMaps(venueLocation);
-      });
-      markers.push(marker);
-    });
-  }
-
-  function removeMarkers() {
-    markers.forEach(function(marker) {
-      marker.setMap(null);
-    });
-    markers.length = 0;
-  }
-
-  $scope.eraseLocation = function() {
-    $scope.coordinates.longitude = null;
-    $scope.coordinates.latitude = null;
-    $scope.filters.address = "";
-  };
-
-  $scope.getVenues = function() {
-    var currentPlace = $scope.autocomplete.getPlace();
-
-    if (currentPlace && currentPlace.geometry) {
-      $scope.coordinates.latitude = currentPlace.geometry.location.lat();
-      $scope.coordinates.longitude = currentPlace.geometry.location.lng();
     }
 
-    if (!$scope.coordinates.longitude || !$scope.coordinates.latitude) {
-      $scope.recommendedPlaces.length = 0;
-    } else {
-      dataService
-        .getVenues({
-          longitude: $scope.coordinates.longitude,
-          latitude: $scope.coordinates.latitude,
-          radius: $scope.filters.radius,
-          section: $scope.filters.section
+    //get user current location, fill the adddress, get the recommended venues
+    function getUserLocation() {
+        geolocationService.getCurrentPosition().then(function(response) {
+            setUserCoordinates(response.coords.latitude, response.coords.longitude);
+            geolocationService
+                .getAddressName(response.coords.latitude, response.coords.longitude)
+                .then(function(result) {
+                    $scope.filters.address = result;
+                    $scope.getVenues();
+                    initMap();
+                })
+        });
+    }
+
+    // initialization of  the map
+    function initMap() {
+        $scope.map = new $window.google.maps.Map(
+            $window.document.getElementById("map"), {
+                zoom: 13,
+                center: {
+                    lat: $scope.coordinates.latitude,
+                    lng: $scope.coordinates.longitude
+                }
+            }
+        );
+    }
+
+    // adding a marker for each recommendede place
+    function addMarkersToMap() {
+        removeMarkers();
+        $scope.recommendedPlaces.forEach(function(element) {
+            var venue = element.venue;
+
+            var marker = new $window.google.maps.Marker({
+                position: {
+                    lat: venue.location.lat,
+                    lng: venue.location.lng
+                },
+                icon: 'assets/beehive.png',
+                map: $scope.map
+            });
+            var infowindow = new $window.google.maps.InfoWindow({
+                content: buildMarkerInfo(venue),
+                maxWidth: 260
+            });
+            $window.google.maps.event.addListener(marker, "click", function() {
+                infowindow.open(map, marker);
+            });
+            $scope.markers.push(marker);
+        });
+    }
+
+    //clean up all the markers from map
+    function removeMarkers() {
+        $scope.markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        $scope.markers.length = 0;
+    }
+
+    //setting the user coordinates
+    function setUserCoordinates(lat, long) {
+        $scope.coordinates.latitude = lat;
+        $scope.coordinates.longitude = long;
+    }
+
+    //concatenate categories of a venue
+    function getCategories(venueCategories) {
+        var categories = "<span>"
+        venueCategories.forEach(function(category) {
+            categories += category.name + '/'
         })
-        .then(function(httpResponse) {
-          $scope.recommendedPlaces = httpResponse.data.response.groups[0].items;
-          addMarkersToMap();
-        });
+
+        return categories + '</span>'
     }
-  };
 
-  function getUserLocation() {
-    geolocationService.getCurrentPosition().then(function(response) {
-      $scope.coordinates = {
-        latitude: response.coords.latitude,
-        longitude: response.coords.longitude
-      };
-      geolocationService
-        .getAddressName(response.coords.latitude, response.coords.longitude)
-        .then(function(result) {
-          $scope.filters.address = result;
-          $scope.getVenues();
-          initMap();
-        });
-    });
-  }
+    //build marker information for map
+    function buildMarkerInfo(venue) {
+        return '<div><h5 class="firstHeading">' +
+            venue.name +
+            '</h5>' +
+            '<div><b>Address</b>:' +
+            '<span>' + venue.location.address + ' , ' + venue.location.city + '</span></div>' +
+            '<div><b>Categories</b>:' + getCategories(venue.categories) +
+            '</div>' +
+            '<a href="https://www.google.com/maps?q=' +
+            venue.location.lat +
+            ',' +
+            venue.location.lng +
+            '">Go to Google Maps</a>' +
+            '</div>';
+    }
 
-  $scope.updateRadius = function(radius) {
-    $scope.filters.radius = radius;
-  };
-
-  $scope.autocompleteAddress = function() {
-    var input = $window.document.getElementById("address");
-    $scope.autocomplete = new $window.google.maps.places.Autocomplete(input);
-  };
+    // set the center of the map
+    function centerMap(lat, lng) {
+        var center = new google.maps.LatLng(lat, lng);
+        $scope.map.panTo(center);
+    }
 }
